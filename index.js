d0c04b7ac6a5893d5bca6061e97634d503947370
Pranjal Upadhyay <pranjalupadhyay13@gmail.com>
2025-03-13 16:26:29 +0530

    nanoid not working redone with shortid

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 
+const urlRoute = require('./routes/url')
 const app = express()
 
 const PORT = 8000
+
+app.use("/url", urlRoute)
 
 app.listen(PORT, () => {
     console.log(`Server Started at PORT ${PORT}`)
---
+++ models/url.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const urlSchema = new mongoose.Schema({
+    shortId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    redirectUrl: {
+        type: String,
+        required: true
+    },
+    visitHistory: [{timestamp: {type: Number}}]
+
+}, {timestamps: true})
+
+const URL = mongoose.model('url', urlSchema)
+//URL is the  constructor function (a Model) that you can use to create and query documents
+//First parameter 'url': This is the name of the collection in MongoDB. MongoDB will automatically create a collection named 'urls' (lowercase and pluralized) in your database.
+
+module.exports = URL
