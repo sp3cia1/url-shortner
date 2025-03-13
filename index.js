b2d56e9511d04009c78697114d52f4af43098288
Pranjal Upadhyay <pranjalupadhyay13@gmail.com>
2025-03-13 16:42:18 +0530

    connected to mongodb

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const {connectToMongoDB} = require('./connect')
 
 const urlRoute = require('./routes/url')
 const app = express()
 
 const PORT = 8000
+
+connectToMongoDB('mongodb://localhost:27017/short-url')
+.then(() => console.log("Mongo DB connected"))
 
 app.use("/url", urlRoute)
 
